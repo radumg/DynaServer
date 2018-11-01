86da9e2ed34756c27a0af9707e95cdb54cdcfb49
Radu Gidei <radugidei@gmail.com>
2018-11-01 16:03:13 +0000

    make top level folders and move server to it

---
+++ Server/DynamoServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+using Topshelf;
+
+namespace DynamoServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting web service on " + DynamoWebServer.URL_BASE);
+            Task.Run(async () => { ServiceRunner.Run(); }).Wait();
+
+            Console.WriteLine("DynamoServer terminated at " + DateTime.UtcNow);
+        }
+    }
+}
+
