c0e3fa4058aa0720b608891b486e62b1152fb607
Radu Gidei <radugidei@gmail.com>
2018-11-03 15:49:46 +0000

    add About & Json example modules to Extension

---
+++ Extensions/Modules/AboutModule.cs
@@ -0,0 +1,26 @@
+﻿using Nancy;
+using System.Reflection;
+
+namespace DynamoServer.Server
+{
+    /// <summary>
+    /// This module only handles requests that target the /about endpoints.
+    /// </summary>
+    public class AboutModule : NancyModule
+    {
+        public AboutModule() : base("/about")
+        {
+            Get["/"] = x =>
+            {
+                var info =
+                    "<h2>DynamoServer</h2></br>" +
+                    "Assembly : " + Assembly.GetExecutingAssembly().FullName + "</br>" +
+                    "Version : " + Assembly.GetExecutingAssembly().GetName().Version + "</br></br>" +
+                    "<h2>Dynamo</h2></br>" +
+                    "Version : " + Dynamo.Utilities.AssemblyHelper.GetDynamoVersion();
+
+                return Response.AsText(info,"text/html");
+            };
+        }
+    }
+}
