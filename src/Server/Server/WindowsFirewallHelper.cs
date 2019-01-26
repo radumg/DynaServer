07596b0cdcfc1d9f6f2ac709e66ebcc70ab8d40b
Radu Gidei <radugidei@gmail.com>
2019-01-26 18:04:22 +0000

    add helper to deal with Windows Firewall

---
+++ src/Server/Server/WindowsFirewallHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NetFwTypeLib;
+
+namespace DynaServer.Utils
+{
+    internal static class WindowsFirewallHelper
+    {
+        internal static bool AddFirewallRule(int port)
+        {
+            INetFwMgr icfMgr = null;
+            try
+            {
+                Type TicfMgr = Type.GetTypeFromProgID("HNetCfg.FwMgr");
+                icfMgr = (INetFwMgr)Activator.CreateInstance(TicfMgr);
+
+                INetFwProfile profile;
+                INetFwOpenPort portClass;
+                Type TportClass = Type.GetTypeFromProgID("HNetCfg.FWOpenPort");
+                portClass = (INetFwOpenPort)Activator.CreateInstance(TportClass);
+
+                // Get the current profile
+                profile = icfMgr.LocalPolicy.CurrentProfile;
+
+                // Set the port properties
+                portClass.Scope = NetFwTypeLib.NET_FW_SCOPE_.NET_FW_SCOPE_ALL;
+                portClass.Enabled = true;
+                portClass.Protocol = NetFwTypeLib.NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+                portClass.Name = "DescriptionOfRule";
+                portClass.Port = port;
+
+                // Add the port to the ICF Permissions List
+                profile.GloballyOpenPorts.Add(portClass);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
