0ac3eeab2201f73eaada5d9a9f1e39208df95cca
Radu Gidei <radugidei@gmail.com>
2019-01-26 16:53:09 +0000

    actually stop server in /stop endpoint

--- src/Server/Modules/IndexModule.cs
+++ src/Server/Modules/IndexModule.cs
@@ -14,6 +14,7 @@ namespace DynaServer.Server
 
             Get["/stop"] = x =>
             {
+                DynamoWebServer.CurrentInstance.Stop();
                 return Response.AsFile("extra/stop.html", "text/html");
             };
 
