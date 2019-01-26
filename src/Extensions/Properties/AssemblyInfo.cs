1fcf47a24c36a1b87d9a93e56da2906683df4b1a
Radu Gidei <radugidei@gmail.com>
2019-01-26 18:31:34 +0000

    add metadata to pkg.json and assembly

--- src/Extensions/Properties/AssemblyInfo.cs
+++ src/Extensions/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynaServerExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dynamo extensions to host & add functionality to a DynamoServer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DynaServerExtensions")]
